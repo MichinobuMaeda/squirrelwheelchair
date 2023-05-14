413ec10d723645bac92de7a5fbab8a0d062fb14c
Michinobu Maeda <michinobumaeda@gmail.com>
2023-05-15 07:45:31 +0900

    test: without custom font

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+// import 'package:flutter/foundation.dart';
+// import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
 import 'config/app_info.dart';
 
 void main() {
+  // GoogleFonts.config.allowRuntimeFetching = false;
+  // LicenseRegistry.addLicense(() async* {
+  //   final license = await rootBundle.loadString('google_fonts/OFL.txt');
+  //   yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  // });
+
   runApp(const MyApp());
 }
 
@@ -11,28 +20,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    const seedColor = Colors.green;
+    final theme = ThemeData(
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: seedColor,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // textTheme: GoogleFonts.sawarabiGothicTextTheme(
+      //   Theme.of(context).textTheme,
+      // ),
+      useMaterial3: true,
+    );
+    final darkTheme = theme.copyWith(
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: seedColor,
+        brightness: Brightness.dark,
+      ),
+    );
+    return MaterialApp(
+      title: '日本語の漢字 道路',
+      theme: theme,
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.system,
+      home: const MyHomePage(title: '日本国民は、正当に選挙された国会における代表者を通じて行動し'),
     );
   }
 }
@@ -82,7 +91,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
